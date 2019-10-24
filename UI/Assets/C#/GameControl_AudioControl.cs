db806f912e64a406588dc14448651e0ae3265759
jason90516 <55227160+jason90516@users.noreply.github.com>
2019-10-24 14:51:01 +0800

    10/24 SS

---
+++ UI/Assets/C#/GameControl_AudioControl.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class GameControl_AudioControl : MonoBehaviour {
+
+    public AudioMixer AM;
+
+	void Start ()
+    {
+        
+	}
+	
+	void Update ()
+    {
+		
+	}
+
+    public void MasterControl(float volume)
+    {
+        AM.SetFloat("Master", volume);
+    }
+
+    public void BGMControl(float volume)
+    {
+        AM.SetFloat("BGM", volume);
+    }
+
+    public void FSXControl(float volume)
+    {
+        AM.SetFloat("FSX", volume);
+    }
+}
