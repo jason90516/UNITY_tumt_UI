7575fcac901297065a8346848483d6abad1023d5
jason90516 <jason90516@gmail.com>
2019-11-14 14:52:59 +0800

    發佈

--- UI/Assets/C#/GameControl_AudioControl.cs
+++ UI/Assets/C#/GameControl_AudioControl.cs
@@ -11,15 +11,15 @@ public class GameControl_AudioControl : MonoBehaviour {
     public Slider loadung;
 
 
-	void Start ()
+    void Start()
     {
-        
-	}
-	
-	void Update ()
+
+    }
+
+    void Update()
     {
-		
-	}
+
+    }
 
     public void MasterControl(float volume)
     {
@@ -38,7 +38,28 @@ public class GameControl_AudioControl : MonoBehaviour {
 
     public void Play()
     {
-        SceneManager.LoadScene("Assault_Rifle_01_Demo");
+        //     SceneManager.LoadScene("Assault_Rifle_01_Demo");
+        StartCoroutine(Change());
+    }
+
+    IEnumerator Change()
+    {
+        AsyncOperation ao = SceneManager.LoadSceneAsync("Assault_Rifle_01_Demo");
+        ao.allowSceneActivation = false;
+        while (ao.isDone == false)
+        {
+            LoaddingText.text = ((ao.progress / 0.9f) * 100).ToString();
+            loadung.value = ao.progress / 0.9f;
+            yield return new WaitForSeconds(0.001f);
+
+        if(ao.progress == 0.9f)
+        {
+            ao.allowSceneActivation = true;
+    
+        }
+        }
 
     }
+
 }
+     
