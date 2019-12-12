0e1140970421e748d4f8b752289074494b2e2d30
jason90516 <55227160+jason90516@users.noreply.github.com>
2019-12-12 14:49:00 +0800

    垃圾衣鞋那每回

---
+++ UI/Assets/C#/阿翔.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class 阿翔 : MonoBehaviour
+{
+    public int HP = 100;
+    public Slider hpslider;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        print(other.gameObject);
+
+
+        if(other.tag=="陷阱")
+        {
+            int d = other.GetComponentsInParent<TRAP>().damage;
+            HP -= d;
+            HP -= 10;
+            hpslider.value = HP;
+        }
+
+    }
+
+
+}
