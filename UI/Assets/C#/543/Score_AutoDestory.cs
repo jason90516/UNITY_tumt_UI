ca0b67ce937d5fbd0fe60a7402eb843ed4872882
jason90516 <55227160+jason90516@users.noreply.github.com>
2019-12-19 15:29:26 +0800

    霸者東塔改起來 今天限定我來了!

---
+++ UI/Assets/C#/543/Score_AutoDestory.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Score_AutoDestory : MonoBehaviour {
+
+    public GameObject OBJ;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if(OBJ == null)
+        {
+            Destroy(gameObject);
+        }
+
+	}
+}
---
+++ UI/Assets/C#/GGTW.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GGTW : MonoBehaviour
+{
+    public int HP = 100;
+    public Slider hpslider;
+
+    public Text textInfinityStones;
+    public int InfinityStonescount;
+    public int InfinityStonesTotal;
+
+    public Text Time_Text;
+    float TTime;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        print(other.gameObject);
+
+
+        if(other.tag=="陷阱")
+        {
+            int d = other.GetComponent<TRAP>().damage;
+            HP -= d;
+            HP -= 10;
+            hpslider.value = HP;
+        }
+
+        if (other.tag == "寶石")
+        {
+            InfinityStonescount++;
+            textInfinityStones.text = "InfinityStones : " + InfinityStonescount + " / " + InfinityStonesTotal;
+            Destroy(other.gameObject);
+        }
+
+        if(other.name == "Goal" && InfinityStonescount == InfinityStonesTotal)
+        {
+            Debug.Log("廢物");
+        }
+    
+    }
+
+    private void Start()
+    {
+        InfinityStonesTotal = GameObject.FindGameObjectsWithTag("寶石").Length;
+        textInfinityStones.text = "InfinityStones : 0 / " + InfinityStonesTotal;
+    }
+
+    private void Update()
+    {
+        TTime += Time.deltaTime;
+        Time_Text.text = "Time : " + TTime.ToString("0.00");
+    }
+
+}
